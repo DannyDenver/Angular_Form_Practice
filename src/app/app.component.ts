655c530df7abd65a8f7395b46a10f57e1786a416
Daniel Taylor <Daniel Taylor>
2017-09-09 16:44:45 -0600

    Validation Message for email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 
@@ -8,12 +8,17 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm // access form before submit, onSubmit doesnt need parameter 
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form: NgForm){
-    console.log("submitted!")
-    console.log(form);
+  // onSubmit(form: NgForm){
+  //   console.log("submitted!")
+  //   console.log(form);
+  // }
+  
+  onSubmit(){
+    console.log(this.signupForm);
   }
 }
